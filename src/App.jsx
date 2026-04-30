51e54c1d39d585043f6a0a062f7fe55e98e743c7
Yamu <yamunabc55@gmail.com>
2026-04-29 22:06:40 -0700

    Completed Week-03 TodoList component Homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
 
+const todos = [
+    { id: 1, title: 'review resources' },
+    { id: 2, title: 'take notes' },
+    { id: 3, title: 'code out app' },
+  ] 
+
 function App() {
   
+  const [todoList, setTodoList] = useState(todos);
 
   return (
     <div>
